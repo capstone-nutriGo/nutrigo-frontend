f5bed42552ff0cd07bb91a30cf504ca43335e3f2
Jaehun <chlwogns55@gmail.com>
2025-12-12 09:28:21 +0900

    'test'

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,10 @@
   import App from "./App.tsx";
   import "./index.css";
 
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
+console.log('[NutriGo] API_BASE_URL =', API_BASE_URL);
+
+
   createRoot(document.getElementById("root")!).render(<App />);
   
